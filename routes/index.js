8ad9ea5242ff70fcc3d877e5abac7c5351171855
bobbohee <parkbohee0705@gmail.com>
2021-07-02 22:59:19 +0900

    [ADD] svg 이미지 로드

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+const box = () => {
+    return `
+        <svg xmlns="http://www.w3.org/2000/svg" width="200" height="50">
+            <rect width="200" height="50" stroke="#333333" stroke-width="5" fill="#f2f2f2"/>
+        </svg>
+    `;
+}
+
 router.get('/', (req, res) => {
-    res.render('index');
+    res.header('Content-Type', 'image/svg+xml');
+    res.send(box());
 });
 
 module.exports = router;
